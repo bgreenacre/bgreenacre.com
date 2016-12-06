c3d7ea64fd9637835c55bfbb1a8f56d47b0253f3
Brian Greenacre <bgreenacre42@gmail.com>
2016-12-05 21:57:51 -0600

    Adding grunt tasks for frontend workflow.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = function ( grunt )
+{
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( "package.json" ),
+        copy: {
+            dist: {
+                src: "node_modules/jquery/dist/jquery.min.js",
+                dest: "public/js/jquery.min.js"
+            },
+            dev: {
+                src: "node_modules/jquery/dist/jquery.js",
+                dest: "public/js/jquery.js"
+            }
+        },
+        less: {
+            all: {
+                files: [ {
+                    src: [ "resources/less/main.less" ],
+                    dest: "public/css/main.css",
+                } ]
+            }
+        },
+        watch: {
+            less: {
+                files: [ "resources/less/*.less" ],
+                tasks: [ "less" ]
+            },
+        }
+    } );
+
+    grunt.loadNpmTasks( "grunt-contrib-less" );
+    grunt.loadNpmTasks( "grunt-contrib-copy" );
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+    grunt.registerTask( "default", [ "less", "copy", "watch" ] );
+};
